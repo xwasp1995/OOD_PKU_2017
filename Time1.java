6618a3183d74370d71bc000fba53b16bba72106e
Mike <xwasp1995@163.com>
2018-03-09 15:47:23 +0800

    add Time1.java

---
+++ Time1.java
@@ -0,0 +1,10 @@
+public class Time1{
+    private int second = 1000;
+    private int minute;
+    private int hour;
+    
+    public static void main(String[] args){
+	int second = 1000;
+	System.out.println(second/60);
+    }
+}
